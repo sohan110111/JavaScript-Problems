bb31a60ebe632bff8769f64e838beb7b72fc6e0e
sohan110111 <sohan110111@gmail.com>
2020-03-03 16:02:24 +0600

    round-number

---
+++ round.js
@@ -0,0 +1,12 @@
+var number = 2.01;
+var result = Math.round(number);
+console.log(result);
+
+var number2 = 2.49;
+console.log(Math.round(number2));
+
+var number3 = 2.5;
+console.log(Math.round(number3));
+
+var number4 = 2.99;
+console.log(Math.round(number4));
