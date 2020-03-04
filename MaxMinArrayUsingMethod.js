2b5e8ac929e47257772971df563f2f7d1fb56ecf
sohan110111 <sohan110111@gmail.com>
2020-03-04 12:59:22 +0600

    find maximum and minimum value in arry using method (minValue = Math.min(...array); )

---
+++ MaxMinArrayUsingMethod.js
@@ -0,0 +1,6 @@
+var values = [78, 87, 78, 48, 67, 45, 67, 45, 78]; // suppose student get mark in programming course
+console.log(values);
+minValue = Math.min(...values);
+console.log("Minimum value is : ",minValue); 
+maxValue = Math.max(...values); 
+console.log("Maximum value is : ",maxValue);
