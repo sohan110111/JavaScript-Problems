8c9cb0663d7c0ec2e41cd27cac3c8ec48e5bb140
sohan110111 <sohan110111@gmail.com>
2020-03-04 21:29:05 +0600

    Array value of string addition

---
+++ stringAdditionInArray.js
@@ -0,0 +1,9 @@
+var friends = ["Alu", "Bulu", "culu", "dulu", "fulu"];
+
+var stringAdd = friends[0];
+for(var i = 1; i < friends.length; i++) {
+    stringAdd = stringAdd + friends[i];
+}
+console.log(stringAdd);
+
+
