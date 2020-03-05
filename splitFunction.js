1cb5282ae8b5802631da16bd7601a006b4335be4
sohan110111 <sohan110111@gmail.com>
2020-03-06 01:23:08 +0600

    reverse with split function

--- splitFunction.js
+++ splitFunction.js
@@ -6,3 +6,8 @@ var reverse = sentence.split('').reverse().join(""); // use join('')
 console.log(reverse);
 
 console.log("McDonalds".split("").reverse().join(" ")); // code from StackOverFlow
+
+// here given below an example from w3school.com
+var str = "How are you doing today?";
+var res = str.split("");
+console.log(res);
